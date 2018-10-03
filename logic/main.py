588caabb69bf391faf317c8ec730795c13538183
Pijnenburg,Casper C.A.P <357629@student.fontys.nl>
2018-10-03 15:43:04 +0200

    added logger

--- logic/main.py
+++ logic/main.py
@@ -1,6 +1,7 @@
 import utillities.socket_protocol as socket_protocol
 import argparse
 
+from utillities.logger import Logger
 from utillities.socket_protocol import Command, CommandType
 from utillities.classes.socket_client import SocketClient
 from utillities.classes.socket_server import SocketServer
@@ -11,7 +12,7 @@ detector = None
 ledstrip_client = None
 socket_server = None
 is_test = False
-
+log = Logger(__name__)
 
 def close():
     set_to_black_cmd = Command.create(CommandType.set_color, '(0, 0, 0)').serialize()
@@ -22,7 +23,7 @@ def close():
 
 
 def handle_emotion(emotion):
-    command = Command.create(CommandType.set_color,str(emotion.color)).serialize()
+    command = Command.create(CommandType.set_color, str(emotion.color)).serialize()
     ledstrip_client.send(command)
 
 
@@ -41,19 +42,25 @@ def handle_socket_message(message, sender):
 
 def start_server():
     global socket_server
-
+    
     server_config = get_server_config('LOGIC_SERVER', is_test)
+
+    log.debug('Starting LOGIC_SERVER at' + str(server_config))
 
     socket_server = SocketServer(server_config.port, "LOGIC_SERVER")
     socket_server.message_received(handle_socket_message)
     socket_server.start()
+
+    log.debug('LOGIC_SERVER Started')
 
 
 def start_ledstrip_client():
     global ledstrip_client
 
     server_config = get_server_config('LED_SERVER', is_test)
+    log.debug('Connecting to LED_SERVER at' + str(server_config))
     ledstrip_client = SocketClient(server_config.host, server_config.port)
+    log.debug('LED_SERVER Connected')
 
 
 def init_detector():
