ee7dc625b5bd1b3c5c89e47dd77adead16a185c3
Pijnenburg,Casper C.A.P <357629@student.fontys.nl>
2018-10-03 15:17:24 +0200

    only one face

--- logic/ai/emotion_detector.py
+++ logic/ai/emotion_detector.py
@@ -16,7 +16,7 @@ class EmotionDetector():
             kwargs["emotion_model_path"], compile=False)
         self.emotion_target_size = self.emotion_classifier.input_shape[1:3]
         self.emotion_cache = []
-        self.min_cache_size = 12
+        self.min_cache_size = 10
         self.previous_emotion = ''
         self.is_running = False
 
@@ -62,29 +62,26 @@ class EmotionDetector():
                 continue
 
             gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
-            faces = self.face_detection.detectMultiScale(gray_image, 1.3, 5)
+            face = self.face_detection.detectMultiScale(gray_image, 1.3, 5)[0]
 
-            for face_coordinates in faces:
-                x1, x2, y1, y2 = apply_offsets(
-                    face_coordinates, self.emotion_offsets)
-                gray_face = gray_image[y1:y2, x1:x2]
+            x1, x2, y1, y2 = apply_offsets(face, self.emotion_offsets)
+            gray_face = gray_image[y1:y2, x1:x2]
 
-                try:
-                    gray_face = cv2.resize(
-                        gray_face, (self.emotion_target_size))
-                except:
-                    continue
+            try:
+                gray_face = cv2.resize(gray_face, (self.emotion_target_size))
+            except:
+                continue
 
-                gray_face = preprocess_input(gray_face, True)
-                gray_face = np.expand_dims(gray_face, 0)
-                gray_face = np.expand_dims(gray_face, -1)
+            gray_face = preprocess_input(gray_face, True)
+            gray_face = np.expand_dims(gray_face, 0)
+            gray_face = np.expand_dims(gray_face, -1)
 
-                emotion_prediction = self.emotion_classifier.predict(gray_face)
+            emotion_prediction = self.emotion_classifier.predict(gray_face)
 
-                emotion_label_arg = np.argmax(emotion_prediction)
-                emotion = basic_emotions[emotion_label_arg]
+            emotion_label_arg = np.argmax(emotion_prediction)
+            emotion = basic_emotions[emotion_label_arg]
 
-                self.emotion_detected(emotion)
+            self.emotion_detected(emotion)
 
             if cv2.waitKey(1) == 27:
                 break  # esc to break
