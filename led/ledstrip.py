e7f93b09c42a7b5af1f9213c1237afe40d174281
Pijnenburg,Casper C.A.P <357629@student.fontys.nl>
2018-10-04 10:56:50 +0200

    vbn

--- led/ledstrip.py
+++ led/ledstrip.py
@@ -12,13 +12,14 @@ class LedStrip:
     def __init__(self):
         self.controller = ws.PixelStrip(lc.LED_COUNT, lc.LED_PIN, lc.LED_FREQ_HZ, lc.LED_DMA, lc.LED_INVERT, lc.LED_BRIGHTNESS, lc.LED_CHANNEL)
         self.is_on = False
+        self.controller.begin()
         self.current_color = (255, 255, 255)
-        self.transition_to((0, 0, 0), 100)
+        self.transition_to((0,0,0), 100)
 
     def start(self):
         assert not self.is_on
+        
 
-        self.controller.begin()
         self.is_on = True
 
     def stop(self):
