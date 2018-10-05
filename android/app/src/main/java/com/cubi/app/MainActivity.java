759a4559cb5a6bb976b5c12183b90ad5c7222b70
freek van Dijk <f.vandijk@student.fontys.nl>
2018-10-05 11:02:25 +0200

    main nieuwe sockets gemaakt

--- android/app/src/main/java/com/cubi/app/MainActivity.java
+++ android/app/src/main/java/com/cubi/app/MainActivity.java
@@ -2,6 +2,9 @@ package com.cubi.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +14,20 @@ public class MainActivity extends AppCompatActivity {
     private Socket_Client socket_io;
     private Socket_Client socket_logic;
 
+    TextView textResponse;
+    EditText editTextAddress, editTextPort;
+    Button buttonConnect, buttonClear;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextAddress = (EditText)findViewById(R.id.address);
+        editTextPort = (EditText)findViewById(R.id.port);
+        buttonConnect = (Button)findViewById(R.id.connect);
+        buttonClear = (Button)findViewById(R.id.clear);
+        textResponse = (TextView)findViewById(R.id.response);
 
         socket_io = new Socket_Client("192.168.70.146", 8002);
         socket_logic = new Socket_Client("192.168.70.57", 8001);
