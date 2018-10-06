9b074245fae3f564b103c7f0f458a383decb7c9b
Pijnenburg,Casper C.A.P <357629@student.fontys.nl>
2018-10-06 12:08:27 +0200

    Load colors from file

--- remote/Cubi.Remote/Colors.cs
+++ remote/Cubi.Remote/Colors.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace Cubi.Remote
 {
     public class Colors
     {
-        private static readonly Dictionary<string, Tuple<int, int, int>> RgbColors = new Dictionary<string, Tuple<int, int, int>>
-        {
-            { "white", Tuple.Create(255, 255, 255) }
-        };
+        private static readonly Dictionary<string, Tuple<int, int, int>> RgbColors = JsonConvert
+            .DeserializeObject<IEnumerable<Color>>(File.ReadAllText("../../../../config/colors.json"))
+            .ToDictionary(k => k.Name, v => Tuple.Create(v.Rgb[0], v.Rgb[1], v.Rgb[2]));
 
         public static string Get(string name)
         {
@@ -17,4 +20,11 @@ namespace Cubi.Remote
             return color.ToString();
         }
     }
+
+    public class Color
+    {
+        public string Name { get; set; }
+        public int[] Rgb { get; set; }
+    }
+
 }
