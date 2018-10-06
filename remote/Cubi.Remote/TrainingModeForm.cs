24399e082d5bcdf619ec1e1225487ea6fc91320e
Pijnenburg,Casper C.A.P <357629@student.fontys.nl>
2018-10-06 15:50:12 +0200

    Added features

--- remote/Cubi.Remote/TrainingModeForm.cs
+++ remote/Cubi.Remote/TrainingModeForm.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using Cubi.Remote.Winforms;
+using CommandType = Cubi.Remote.Winforms.CommandType;
 
 namespace Cubi.Remote
 {
@@ -15,6 +13,56 @@ namespace Cubi.Remote
         public TrainingModeForm()
         {
             InitializeComponent();
+            InitEmotionList();
+        }
+
+        private void InitEmotionList()
+        {
+            cbxEmotionList.Items.AddRange(
+                Colors.RgbColors
+                    .Select(c => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(c.Emotion.ToLower()))
+                    .ToArray()
+            );
+
+            cbxEmotionList.SelectedIndex = 0;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!(cbxEmotionList.SelectedItem is string emotion)) return;
+
+            string color = Colors.Get(c => c.Emotion == emotion.ToLower());
+            const string black = "(0,0,0)";
+            const int seconds = 3;
+            const int delay = 1000;
+
+            for (int i = 0; i < seconds; i++)
+            {
+                Command
+                    .Create(CommandType.SetLed, color)
+                    .SendTo(WebSockets.IoClient);
+
+                AutoClosingMessageBox.Show((seconds - i).ToString(), $"Emotion: {emotion}", delay);
+
+                Command
+                    .Create(CommandType.SetLed, black)
+                    .SendTo(WebSockets.IoClient);
+
+                Thread.Sleep(delay);
+            }
+
+            string resultColor = MessageBox.Show("Was the emotion correct?", "Result", MessageBoxButtons.YesNo) == DialogResult.Yes
+                ? "green"
+                : "red";
+
+            Command
+                .Create(CommandType.SetLed, Colors.Get(c => c.Name == resultColor))
+                .SendTo(WebSockets.IoClient);
         }
     }
 }
